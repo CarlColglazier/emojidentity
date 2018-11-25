caf20e3d2ba58707438a045b5e57ed131a9ec3af
thesmartwon <thesmartwon@live.com>
2018-11-25 16:22:42 -0500

    Add users we care about

--- scripts/twitter_data.py
+++ scripts/twitter_data.py
@@ -1,8 +1,23 @@
-import twitter
-from secret import *
+import configparser
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=AT_KEY,
-                  access_token_secret=AT_SECRET,
+import twitter
+import pandas as pd
+# from secret import *
+
+def get_api():
+    config = configparser.ConfigParser()
+    config.read('twitter.cfg')
+    return twitter.Api(consumer_key=config["default"]["consumerKey"],
+                  consumer_secret=config["default"]["consumerSecret"],
+                  access_token_key=config["default"]["accessToken"],
+                  access_token_secret=config["default"]["accessTokenSecret"],
                   sleep_on_rate_limit=True)
+
+if __name__ == '__main__':
+    df = pd.read_csv("../data/emoji_users_201811251616.csv", encoding="utf-8")
+    api = get_api()
+    for r in df.iterrows():
+        print(r)
+    # api.get_friends(user_id=132)
+    # print(dir(api))
+
