62a2173eb1598c54eb675fc2e2b7aaa3c4712016
thesmartwon <thesmartwon@live.com>
2018-11-25 16:38:59 -0500

    Write&run loop

--- scripts/twitter_data.py
+++ scripts/twitter_data.py
@@ -4,20 +4,31 @@ import twitter
 import pandas as pd
 # from secret import *
 
-def get_api():
+def get_api(keynum):
     config = configparser.ConfigParser()
-    config.read('twitter.cfg')
+    config.read('twitter' + str(keynum) + '.cfg')
     return twitter.Api(consumer_key=config["default"]["consumerKey"],
                   consumer_secret=config["default"]["consumerSecret"],
                   access_token_key=config["default"]["accessToken"],
                   access_token_secret=config["default"]["accessTokenSecret"],
                   sleep_on_rate_limit=True)
 
+
 if __name__ == '__main__':
     df = pd.read_csv("../data/emoji_users_201811251616.csv", encoding="utf-8")
-    api = get_api()
-    for r in df.iterrows():
-        print(r)
-    # api.get_friends(user_id=132)
-    # print(dir(api))
+    apis = [get_api(i) for i in range(2)]
+    try:
+        for i, row in df.iterrows():
+            api = apis[i % len(apis)]
+            uid = int(row["id"])
+            print(i, "Getting followers/following for", uid)
+            # print(row["id"])
+            followers = api.GetFollowers(user_id=uid)
+            following = api.GetFriends(user_id=uid)
+            df.loc[i,"followers"] = ",".join(str(f.id) for f in followers)
+            df.loc[i,"following"] = ",".join(str(f.id) for f in following)
+    finally:
+        print(df)
+        df.to_csv("../data/results.csv", index=False)
+        # print(dir(api))
 
