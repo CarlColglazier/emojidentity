c292222f8f81c913935ad584bbfe39f7a41410e9
thesmartwon <thesmartwon@live.com>
2018-11-27 14:41:41 -0500

    Control group followers/following

--- scripts/twitter_data.py
+++ scripts/twitter_data.py
@@ -1,34 +1,43 @@
 import configparser
+import time
+import math
 
-import twitter
+from twython import Twython
 import pandas as pd
 # from secret import *
 
 def get_api(keynum):
     config = configparser.ConfigParser()
     config.read('twitter' + str(keynum) + '.cfg')
-    return twitter.Api(consumer_key=config["default"]["consumerKey"],
-                  consumer_secret=config["default"]["consumerSecret"],
-                  access_token_key=config["default"]["accessToken"],
-                  access_token_secret=config["default"]["accessTokenSecret"],
-                  sleep_on_rate_limit=True)
+    return Twython(config["default"]["consumerKey"],
+                  config["default"]["consumerSecret"],
+                  config["default"]["accessToken"],
+                  config["default"]["accessTokenSecret"])
+
 
 
 if __name__ == '__main__':
-    df = pd.read_csv("../data/emoji_users_201811251616.csv", encoding="utf-8")
+    df = pd.read_csv("../data/results.csv", encoding="utf-8")
     apis = [get_api(i) for i in range(2)]
     try:
-        for i, row in df.iterrows():
+        for i, row in df.iloc[[162 ,1082,1318,1532,1542,1600,1778]].iterrows():
             api = apis[i % len(apis)]
             uid = int(row["id"])
+            if type(row["followers"]) is str:
+                print("Skipping", uid)
+                continue
             print(i, "Getting followers/following for", uid)
-            # print(row["id"])
-            followers = api.GetFollowers(user_id=uid)
-            following = api.GetFriends(user_id=uid)
-            df.loc[i,"followers"] = ",".join(str(f.id) for f in followers)
-            df.loc[i,"following"] = ",".join(str(f.id) for f in following)
+            try:
+                followers = api.get_followers_ids(user_id=uid)
+                following = api.get_friends_ids(user_id=uid)
+            except Exception as e:
+                print("failed:", e)
+                continue
+            finally:
+                time.sleep(math.ceil(60 / len(apis))+5)
+            df.loc[i,"followers"] = ",".join(str(f) for f in followers["ids"])
+            df.loc[i,"following"] = ",".join(str(f) for f in following["ids"])
     finally:
-        print(df)
+        # print(df)
         df.to_csv("../data/results.csv", index=False)
         # print(dir(api))
-
